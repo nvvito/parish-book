9c3f1865558326d68cb97c83cbd478eb5415c65f
Viktor Nakaznyi <viktornakaznyi@Viktors-MacBook-Pro.local>
2021-01-19 23:07:04 +0200

    fix gitignore file in client folder

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(express.static('public/client/build'));
 // server app routes
 app.use(router);
 // start server app
